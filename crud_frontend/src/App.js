440c3033fdfc54d5e02ac910bd5af5caeab7a955
WAG395 <158130936+WAG395@users.noreply.github.com>
2024-04-08 11:31:44 +0530

    added logo

--- crud_frontend/src/App.js
+++ crud_frontend/src/App.js
@@ -1,13 +1,13 @@
-
 import './App.css';
+import logo from './mylogo.png'; // Import the logo image
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+        <img src={logo} className="App-logo" alt="logo" /> {/* Logo image */}
         <h1>
-          Welcome To User-Link !
+          Welcome To User-Link!
         </h1>
         <button className="users-button"><h1>Users</h1></button>
       </header>
